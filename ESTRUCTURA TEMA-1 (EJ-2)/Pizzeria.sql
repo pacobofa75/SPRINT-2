05ba9a733697c7537dbc132623d864fb6f21b874
pacobofa75 <pacobofa@gmail.com>
2023-10-18 10:26:42 +0200

    Correcciones Octubre 18

--- ESTRUCTURA TEMA-1 (EJ-2)/Pizzeria.sql
+++ ESTRUCTURA TEMA-1 (EJ-2)/Pizzeria.sql
@@ -19,13 +19,13 @@ USE `mydb` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Client` (
   `Id_cliente` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(45) NULL,
-  `Apellido` VARCHAR(45) NULL,
-  `Direccion` VARCHAR(45) NULL,
-  `codigo_postal` INT NULL,
-  `Localidad` VARCHAR(45) NULL,
-  `Provicnia` VARCHAR(45) NULL,
-  `Telefono` INT NULL,
+  `Nombre` VARCHAR(45) NOT NULL,
+  `Apellido` VARCHAR(45) NOT NULL,
+  `Direccion` VARCHAR(45) NOT NULL,
+  `codigo_postal` INT NOT NULL,
+  `Localidad` VARCHAR(45) NOT NULL,
+  `Provicnia` VARCHAR(45) NOT NULL,
+  `Telefono` INT NOT NULL,
   PRIMARY KEY (`Id_cliente`))
 ENGINE = InnoDB
 COMMENT = '					';
@@ -45,21 +45,26 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Empleat`
+-- Table `mydb`.`Comanda`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Empleat` (
-  `Id_empleat` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(45) NULL,
-  `Apellido_1` VARCHAR(45) NULL,
-  `Apellido_2` VARCHAR(45) NULL,
-  `NIF` INT NULL,
-  `Telefon` INT NULL,
-  `Tipus` ENUM('cuiner', 'repartidor') NULL,
-  `Botiga_Id_botiga` INT NOT NULL,
-  PRIMARY KEY (`Id_empleat`, `Botiga_Id_botiga`),
-  INDEX `fk_Empleat_Botiga1_idx` (`Botiga_Id_botiga` ASC) VISIBLE,
-  CONSTRAINT `fk_Empleat_Botiga1`
-    FOREIGN KEY (`Botiga_Id_botiga`)
+CREATE TABLE IF NOT EXISTS `mydb`.`Comanda` (
+  `Id_comanda` INT NOT NULL AUTO_INCREMENT,
+  `Fecha_pedido` DATE NOT NULL,
+  `tipo_venta` ENUM('reparto', 'recoger') NOT NULL,
+  `Cantidad_productos` INT NOT NULL,
+  `Precio` INT NOT NULL,
+  `iId_cliente` INT NOT NULL,
+  `Id_botiga` INT NOT NULL,
+  PRIMARY KEY (`Id_comanda`),
+  INDEX `fk_Comanda_Client1_idx` (`iId_cliente` ASC) VISIBLE,
+  INDEX `fk_Comanda_Botiga1_idx` (`Id_botiga` ASC) VISIBLE,
+  CONSTRAINT `fk_Comanda_Client1`
+    FOREIGN KEY (`iId_cliente`)
+    REFERENCES `mydb`.`Client` (`Id_cliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Comanda_Botiga1`
+    FOREIGN KEY (`Id_botiga`)
     REFERENCES `mydb`.`Botiga` (`Id_botiga`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -86,11 +91,11 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Producte` (
   `Imagen` VARCHAR(45) NULL,
   `Precio` INT NULL,
   `Tipo` ENUM('pizza', 'hamburguesa', 'beguda') NULL,
-  `Categoria_Id_categoria` INT NOT NULL,
+  `Id_categoria` INT NOT NULL,
   PRIMARY KEY (`ID_Producte`, `Descripcion`),
-  INDEX `fk_Producte_Categoria1_idx` (`Categoria_Id_categoria` ASC) VISIBLE,
+  INDEX `fk_Producte_Categoria1_idx` (`Id_categoria` ASC) VISIBLE,
   CONSTRAINT `fk_Producte_Categoria1`
-    FOREIGN KEY (`Categoria_Id_categoria`)
+    FOREIGN KEY (`Id_categoria`)
     REFERENCES `mydb`.`Categoria` (`Id_categoria`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -98,45 +103,71 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Comanda`
+-- Table `mydb`.`Entrega`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Comanda` (
-  `Id_comanda` INT NOT NULL AUTO_INCREMENT,
-  `Fecha` DATE NULL,
-  `tipo_venta` ENUM('reparto', 'recoger') NULL,
-  `Cantidad_productos` INT NULL,
-  `Precio` INT NULL,
-  `Id_cliente` INT NULL,
-  `Tipo_Productos` ENUM('pizza', 'hamburguesa', 'beguda') NULL,
-  `Client_Id_cliente` INT NOT NULL,
-  `Botiga_Id_botiga` INT NOT NULL,
-  `Empleat_Id_empleat` INT NOT NULL,
-  `Empleat_Botiga_Id_botiga` INT NOT NULL,
-  `Producte_ID_Producte` INT NOT NULL,
-  `Producte_Descripcion` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Id_comanda`),
-  INDEX `fk_Comanda_Client1_idx` (`Client_Id_cliente` ASC) VISIBLE,
-  INDEX `fk_Comanda_Botiga1_idx` (`Botiga_Id_botiga` ASC) VISIBLE,
-  INDEX `fk_Comanda_Empleat1_idx` (`Empleat_Id_empleat` ASC, `Empleat_Botiga_Id_botiga` ASC) VISIBLE,
-  INDEX `fk_Comanda_Producte1_idx` (`Producte_ID_Producte` ASC, `Producte_Descripcion` ASC) VISIBLE,
-  CONSTRAINT `fk_Comanda_Client1`
-    FOREIGN KEY (`Client_Id_cliente`)
-    REFERENCES `mydb`.`Client` (`Id_cliente`)
+CREATE TABLE IF NOT EXISTS `mydb`.`Entrega` (
+  `id_entrega` INT NOT NULL,
+  `id_empleado` INT NOT NULL,
+  `fecha_entrega` DATETIME NOT NULL,
+  `id_comanda` INT NOT NULL,
+  PRIMARY KEY (`id_entrega`),
+  INDEX `fk_Entrega_Comanda1_idx` (`id_comanda` ASC) VISIBLE,
+  CONSTRAINT `fk_Entrega_Comanda1`
+    FOREIGN KEY (`id_comanda`)
+    REFERENCES `mydb`.`Comanda` (`Id_comanda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Empleat`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Empleat` (
+  `Id_empleat` INT NOT NULL AUTO_INCREMENT,
+  `Nombre` VARCHAR(45) NULL,
+  `Apellido_1` VARCHAR(45) NULL,
+  `Apellido_2` VARCHAR(45) NULL,
+  `Nif` INT NULL,
+  `Telefon` INT NULL,
+  `Tipus` ENUM('cuiner', 'repartidor') NULL,
+  `id_entrega` INT NOT NULL,
+  `Id_botiga` INT NOT NULL,
+  PRIMARY KEY (`Id_empleat`),
+  INDEX `fk_Empleat_Entrega1_idx` (`id_entrega` ASC) VISIBLE,
+  INDEX `fk_Empleat_Botiga1_idx` (`Id_botiga` ASC) VISIBLE,
+  CONSTRAINT `fk_Empleat_Entrega1`
+    FOREIGN KEY (`id_entrega`)
+    REFERENCES `mydb`.`Entrega` (`id_entrega`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Comanda_Botiga1`
-    FOREIGN KEY (`Botiga_Id_botiga`)
+  CONSTRAINT `fk_Empleat_Botiga1`
+    FOREIGN KEY (`Id_botiga`)
     REFERENCES `mydb`.`Botiga` (`Id_botiga`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Comanda_Empleat1`
-    FOREIGN KEY (`Empleat_Id_empleat` , `Empleat_Botiga_Id_botiga`)
-    REFERENCES `mydb`.`Empleat` (`Id_empleat` , `Botiga_Id_botiga`)
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`TicketComanda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`TicketComanda` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_Producte` INT NOT NULL,
+  `Id_comanda` INT NOT NULL,
+  `Cantidad` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_InfoPedido_Producte1_idx` (`id_Producte` ASC) VISIBLE,
+  INDEX `fk_InfoPedido_Comanda1_idx` (`Id_comanda` ASC) VISIBLE,
+  CONSTRAINT `fk_InfoPedido_Producte1`
+    FOREIGN KEY (`id_Producte`)
+    REFERENCES `mydb`.`Producte` (`ID_Producte`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Comanda_Producte1`
-    FOREIGN KEY (`Producte_ID_Producte` , `Producte_Descripcion`)
-    REFERENCES `mydb`.`Producte` (`ID_Producte` , `Descripcion`)
+  CONSTRAINT `fk_InfoPedido_Comanda1`
+    FOREIGN KEY (`Id_comanda`)
+    REFERENCES `mydb`.`Comanda` (`Id_comanda`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -171,13 +202,13 @@ COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `mydb`.`Empleat`
+-- Data for table `mydb`.`Comanda`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `mydb`;
-INSERT INTO `mydb`.`Empleat` (`Id_empleat`, `Nombre`, `Apellido_1`, `Apellido_2`, `NIF`, `Telefon`, `Tipus`, `Botiga_Id_botiga`) VALUES (1, 'Luis', 'Garcia', 'Perez', 12312312A, 9301010101010, 'cuiner', DEFAULT);
-INSERT INTO `mydb`.`Empleat` (`Id_empleat`, `Nombre`, `Apellido_1`, `Apellido_2`, `NIF`, `Telefon`, `Tipus`, `Botiga_Id_botiga`) VALUES (2, 'Pedro', 'Marquez', 'Lopez', 52453456B, 9312312343, 'repartidor', DEFAULT);
-INSERT INTO `mydb`.`Empleat` (`Id_empleat`, `Nombre`, `Apellido_1`, `Apellido_2`, `NIF`, `Telefon`, `Tipus`, `Botiga_Id_botiga`) VALUES (3, 'Maria', 'Galindo', 'Fernandez', 12312312E, 9312312333, 'cuiner', DEFAULT);
+INSERT INTO `mydb`.`Comanda` (`Id_comanda`, `Fecha_pedido`, `tipo_venta`, `Cantidad_productos`, `Precio`, `iId_cliente`, `Id_botiga`) VALUES (1, '2022-06-12', 'reparto', 3, 30, DEFAULT, DEFAULT);
+INSERT INTO `mydb`.`Comanda` (`Id_comanda`, `Fecha_pedido`, `tipo_venta`, `Cantidad_productos`, `Precio`, `iId_cliente`, `Id_botiga`) VALUES (2, '2021-08-05', 'recoger', 4, 25, DEFAULT, DEFAULT);
+INSERT INTO `mydb`.`Comanda` (`Id_comanda`, `Fecha_pedido`, `tipo_venta`, `Cantidad_productos`, `Precio`, `iId_cliente`, `Id_botiga`) VALUES (3, '2020-11-23', 'recoger', 1, 20, DEFAULT, DEFAULT);
 
 COMMIT;
 
@@ -200,21 +231,21 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `mydb`;
-INSERT INTO `mydb`.`Producte` (`ID_Producte`, `Nombre`, `Descripcion`, `Imagen`, `Precio`, `Tipo`, `Categoria_Id_categoria`) VALUES (1, 'hawaiana', 'pizza de jamon y piña', NULL, 20, 'pìzza', DEFAULT);
-INSERT INTO `mydb`.`Producte` (`ID_Producte`, `Nombre`, `Descripcion`, `Imagen`, `Precio`, `Tipo`, `Categoria_Id_categoria`) VALUES (2, 'completa', 'hamburguesa con todo', NULL, 12, 'hamburguesa', DEFAULT);
-INSERT INTO `mydb`.`Producte` (`ID_Producte`, `Nombre`, `Descripcion`, `Imagen`, `Precio`, `Tipo`, `Categoria_Id_categoria`) VALUES (3, 'con gas', 'bebida con gas', NULL, 2, 'beguda', DEFAULT);
+INSERT INTO `mydb`.`Producte` (`ID_Producte`, `Nombre`, `Descripcion`, `Imagen`, `Precio`, `Tipo`, `Id_categoria`) VALUES (1, 'hawaiana', 'pizza de jamon y piña', NULL, 20, 'pìzza', DEFAULT);
+INSERT INTO `mydb`.`Producte` (`ID_Producte`, `Nombre`, `Descripcion`, `Imagen`, `Precio`, `Tipo`, `Id_categoria`) VALUES (2, 'completa', 'hamburguesa con todo', NULL, 12, 'hamburguesa', DEFAULT);
+INSERT INTO `mydb`.`Producte` (`ID_Producte`, `Nombre`, `Descripcion`, `Imagen`, `Precio`, `Tipo`, `Id_categoria`) VALUES (3, 'con gas', 'bebida con gas', NULL, 2, 'beguda', DEFAULT);
 
 COMMIT;
 
 
 -- -----------------------------------------------------
--- Data for table `mydb`.`Comanda`
+-- Data for table `mydb`.`Empleat`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `mydb`;
-INSERT INTO `mydb`.`Comanda` (`Id_comanda`, `Fecha`, `tipo_venta`, `Cantidad_productos`, `Precio`, `Id_cliente`, `Tipo_Productos`, `Client_Id_cliente`, `Botiga_Id_botiga`, `Empleat_Id_empleat`, `Empleat_Botiga_Id_botiga`, `Producte_ID_Producte`, `Producte_Descripcion`) VALUES (1, '2022-06-12', 'reparto', 3, 30, 2, 'pizza', DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
-INSERT INTO `mydb`.`Comanda` (`Id_comanda`, `Fecha`, `tipo_venta`, `Cantidad_productos`, `Precio`, `Id_cliente`, `Tipo_Productos`, `Client_Id_cliente`, `Botiga_Id_botiga`, `Empleat_Id_empleat`, `Empleat_Botiga_Id_botiga`, `Producte_ID_Producte`, `Producte_Descripcion`) VALUES (2, '2021-08-05', 'recoger', 4, 25, 1, 'hamburguesa', DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
-INSERT INTO `mydb`.`Comanda` (`Id_comanda`, `Fecha`, `tipo_venta`, `Cantidad_productos`, `Precio`, `Id_cliente`, `Tipo_Productos`, `Client_Id_cliente`, `Botiga_Id_botiga`, `Empleat_Id_empleat`, `Empleat_Botiga_Id_botiga`, `Producte_ID_Producte`, `Producte_Descripcion`) VALUES (3, '2020-11-23', 'recoger', 1, 20, 3, 'beguda', DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
+INSERT INTO `mydb`.`Empleat` (`Id_empleat`, `Nombre`, `Apellido_1`, `Apellido_2`, `Nif`, `Telefon`, `Tipus`, `id_entrega`, `Id_botiga`) VALUES (1, 'Luis', 'Garcia', 'Perez', 12312312A, 9301010101010, 'cuiner', DEFAULT, DEFAULT);
+INSERT INTO `mydb`.`Empleat` (`Id_empleat`, `Nombre`, `Apellido_1`, `Apellido_2`, `Nif`, `Telefon`, `Tipus`, `id_entrega`, `Id_botiga`) VALUES (2, 'Pedro', 'Marquez', 'Lopez', 52453456B, 9312312343, 'repartidor', DEFAULT, DEFAULT);
+INSERT INTO `mydb`.`Empleat` (`Id_empleat`, `Nombre`, `Apellido_1`, `Apellido_2`, `Nif`, `Telefon`, `Tipus`, `id_entrega`, `Id_botiga`) VALUES (3, 'Maria', 'Galindo', 'Fernandez', 12312312E, 9312312333, 'cuiner', DEFAULT, DEFAULT);
 
 COMMIT;
 
